ee2d9b36365261be1ae15baf78ecda3fca79d260
Alston Richey <alston.rives.richey@gmail.com>
2025-09-11 12:11:50 -0400

    Removed unnecessary files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Learning/TeleOp_Starter.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.Learning;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@TeleOp(name = "TeleOp_Starter")
+public class TeleOp_Starter extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
