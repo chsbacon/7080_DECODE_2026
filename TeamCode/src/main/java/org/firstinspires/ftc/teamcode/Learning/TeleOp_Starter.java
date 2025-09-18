0f9bfd5b61473ab8950e3f38975b74da86c2d04d
potatos2g <potatos2e@gmail.com>
2025-09-18 14:13:48 -0400

    Update TeleOp_Starter.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Learning/TeleOp_Starter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Learning/TeleOp_Starter.java
@@ -6,12 +6,16 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 @TeleOp(name = "TeleOp_Starter")
 public class TeleOp_Starter extends LinearOpMode{
-  
-        waitForStart();
 
-        while(opModeIsActive()) {
+  @Override
+  public void runOpMode() throws InterruptedException {
+    
+    waitForStart();
+
+    while(opModeIsActive()) {
 
           
 
-        }
+    }
+  }
 }
