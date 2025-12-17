6d01469f0dfe59284f671dfc34e9f164cdc9c029
potatos2g <potatos2e@gmail.com>
2025-12-17 15:43:44 -0500

    This should HOPEFULLY add a servo the the A button on the controller (no promises)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Learning/Servo_Test.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.Learning;
+
+
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+public class Servo_Test extends OpMode{
+    public CRServo servo;
+
+
+
+    @Override
+    public void init() {
+        servo = hardwareMap.get(CRServo.class, "servo");
+    }
+
+
+    @Override
+    public void loop() {
+
+        boolean servoState = false;
+        boolean faceButtonA = gamepad1.a;
+
+         if(faceButtonA && !servoState) {
+
+             servo.setPower(1);
+             servoState = true;
+         }
+
+         if(faceButtonA && servoState) {
+
+             servo.setPower(0);
+             servoState = false;
+
+         }
+
+    }
+
+}
+
