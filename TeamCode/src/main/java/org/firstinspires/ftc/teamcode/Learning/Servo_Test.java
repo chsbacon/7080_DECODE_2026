530d53d8f7bfa3f33a763efb7f61146e32e5e091
potatos2g <potatos2e@gmail.com>
2025-12-18 10:12:09 -0500

    made it actually work this time (hopefully)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Learning/Servo_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Learning/Servo_Test.java
@@ -1,19 +1,18 @@
 package org.firstinspires.ftc.teamcode.Learning;
 
 
-
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 
-public class Servo_Test extends OpMode{
+public class Servo_Test extends OpMode {
     public CRServo servo;
-
 
 
     @Override
     public void init() {
         servo = hardwareMap.get(CRServo.class, "servo");
+
+
     }
 
 
@@ -21,22 +20,28 @@ public class Servo_Test extends OpMode{
     public void loop() {
 
         boolean servoState = false;
+
         boolean faceButtonA = gamepad1.a;
 
-         if(faceButtonA && !servoState) {
 
-             servo.setPower(1);
-             servoState = true;
-         }
+        while(true) { //this will be changed to while(opModeIsActive()) when implemented i just don't want to import allat rn
 
-         if(faceButtonA && servoState) {
+            if (faceButtonA && !servoState) {
 
-             servo.setPower(0);
-             servoState = false;
+                servo.setPower(1);
+                servoState = true;
+            }
 
-         }
+            if (faceButtonA && servoState) {
+
+                servo.setPower(0);
+                servoState = false;
+
+            }
+
+        }
+
 
     }
 
 }
-
