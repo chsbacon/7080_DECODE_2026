cb108481b93e882ddb63a72aad686f7b149badb9
wibwib <ray@dokholyan.org>
2026-01-08 16:39:06 -0500

    uygouyg

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -40,17 +40,17 @@ public class MeepMeepTesting {
 
                         .strafeTo(new Vector2d(-12, 45))
                         .waitSeconds(1)
-                        .splineToSplineHeading(new Pose2d(-31.5,21.5, Math.PI/4), 3*Math.PI/4)
+                        .splineToSplineHeading(new Pose2d(-31.5,21.5, 3 * Math.PI/4), 3*Math.PI/4)
                         .waitSeconds(1)
                         .strafeTo(new Vector2d(-28.9, 21.51))
-                        .splineToSplineHeading(new Pose2d(12, 30, 3*Math.PI/2), Math.PI/2)
+                        .splineToSplineHeading(new Pose2d(12, 30, Math.PI/2), Math.PI/2)
 
                         .strafeTo(new Vector2d(12, 45))
                         .waitSeconds(1)
                 .splineToSplineHeading(new Pose2d(-31.5,21.5, 3*Math.PI/4), 3*Math.PI/4)
                 .waitSeconds(1)
                         .strafeTo(new Vector2d(-28.9, 21.51))
-                        .splineToSplineHeading(new Pose2d(36, 30, 3*Math.PI/2), Math.PI/2)
+                        .splineToSplineHeading(new Pose2d(36, 30, Math.PI/2), Math.PI/2)
 
                         .strafeTo(new Vector2d(36, 45))
                         .waitSeconds(1)
